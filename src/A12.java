51ef1d0f4c758be51b418c2718921858deb9b687
Shehan Atukorala <shehanatukorala@gmail.com>
2019-01-25 09:44:56 -0500

    Update A12.java

--- src/A12.java
+++ src/A12.java
@@ -1,59 +1,15 @@
-import javax.management.MBeanRegistrationException;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.nio.file.*;
+import java.util.*;
 import java.util.regex.*;
-
-
-public class A12 {
-
-
-    public static void main(String[] args) {
-        List<String> keyWords = Arrays.asList("WRITE", "READ", "IF", "ELSE", "RETURN", "BEGIN", "END", "MAIN", "STRING", "INT", "REAL", ";", ",", "(", ")", "+", "-", "*", "/", ":=", "==", "!=", "input\"","input");
-        List<String> storeWords = new ArrayList<>();
-        List<String> store = new ArrayList<>();
-        FileWriter fileWriter;
-        BufferedReader bufferedReader;
-        BufferedWriter bufferedWriter;
-        File file = new File("/Users/shehanatukorala/Desktop/test.txt");
-        //String patternString = "[A-Za-z0-9.\"]";
-        String patternString = "([^;+:=*,_\\- ()#&%!]+)";
-        String test ="";
-        Pattern p = Pattern.compile(patternString);
-        Matcher m;
-        String[] list;
-        String line;
-        String bigString = "";
-        int counter = 0;
-        try {
-            bufferedWriter = new BufferedWriter(new FileWriter("/Users/shehanatukorala/Desktop/A1.output"));
-            bufferedReader = new BufferedReader(new FileReader(file));
-            while ((line = bufferedReader.readLine()) != null) {
-                m = p.matcher(line);
-                while(m.find()){
-                    test = m.group();
-                    if(!keyWords.contains(test) && !storeWords.contains(test) && test.charAt(0) != '\"'){
-                        ++counter;
-                        storeWords.add(test);
-                    }
-                }
-             //   list = line.split(patternString);
-//                for(int i = 0 ; i < list.length;i++){
-//                    if(!keyWords.contains(list[i])){
-//                        System.out.println(list[i]);
-//                        counter++;
-//
-//                    }
-//                }
-            };
-
-            System.out.println("identifiers:"+counter);
-            bufferedReader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
+ class A12Test {
+    public static void main(String[] args) throws java.io.IOException{
+        Set<String> setwords = new HashSet<>();
+        Matcher m = Pattern.compile("(?:\"[^\"]*\")|[a-zA-Z][a-zA-Z0-9]*").matcher(new String(Files.readAllBytes(Paths.get("/Users/shehanatukorala/Desktop/Desktop/test.txt"))));
+        while(m.find()){
+            if(!m.group().matches("(?:\"[^\"]*\")")&& !Arrays.asList(("WRITEZREADZIFZELSEZRETURNZBEGINZENDZMAINZSTRINGZINTZREALZMAIN").split("Z")).contains(m.group())){
+                setwords.add(m.group());
+            }
         }
-
+        Files.write(Paths.get("/Users/shehanatukorala/Desktop/Desktop/testfile.txt"), ("identifiers:" + setwords.size()).getBytes());
     }
-}//"((?:\\"|[^"]|\\")*)" - targest the strings in the double quotes
+}
