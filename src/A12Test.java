d4753e7911659ca85497c788d586b42b0ffbf806
Shehan Atukorala <shehanatukorala@gmail.com>
2019-02-12 09:20:04 -0500

    Update A12Test.java

--- src/A12Test.java
+++ src/A12Test.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.Pattern; 
 
 public class A12Test {
     public static void main(String[] args) {
